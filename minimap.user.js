335ce4e8dcffb5fe5023c2ab381c3df742deea59
AsumaGC <43731551+AsumaGC@users.noreply.github.com>
2018-11-10 01:26:27 +0000

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -18,7 +18,7 @@ Number.prototype.between = function(a, b) {
   return this > min && this < max;
 };
 var range = 25;
-window.baseTepmlateUrl = 'https://raw.githubusercontent.com/AsumaGC/brasl-Map/master/';
+window.baseTepmlateUrl = 'https://raw.githubusercontent.com/AsumaGC/voidmap/master/';
 
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
