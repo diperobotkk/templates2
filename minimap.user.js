419ef2fa71b937cdafe602d754b6d52db1213498
Martín <Tincho2005_92@hotmail.com>
2019-01-28 01:49:29 -0300

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -18,7 +18,7 @@ Number.prototype.between = function(a, b) {
   return this > min && this < max;
 };
 var range = 25;
-window.baseTepmlateUrl = 'https://raw.githubusercontent.com/AsumaGC/NaziZone/master/';
+window.baseTepmlateUrl = 'https://raw.githubusercontent.com/Pinkfloydd/ArgentinaMap_PixelZone/master/';
 
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
