fca5a4d1e0992d5ffe8291d25f9ef1b58873f69d
zAsuma <51724561+zAsuma@users.noreply.github.com>
2019-06-15 00:52:34 +0100

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -64,7 +64,7 @@ window.addEventListener('load', function () {
         '<canvas id="minimap-board" style="width: 100%; height: 100%;z-index:2;position:absolute;top:0;left:0;"></canvas>' +
         '<canvas id="minimap-cursor" style="width: 100%; height: 100%;z-index:3;position:absolute;top:0;left:0;"></canvas>' +
         '</div><div id="minimap-config" style="line-height:20px;">' +
-        		'<a href=https://discord.io/italianempire target="_blank">Discord' +
+        		'<a href=https://discord.io/imperiobrasil target="_blank">Discord' +
         '</a> | <span id="hide-map" style="cursor:pointer;font-weight:bold;color: rgb(255, 255, 0);"> Esconder' +
         '</span> | Zoom: <span id="zoom-plus" style="cursor:pointer;font-weight:bold;color: rgb(0, 255, 0);">+</span>  /  ' +
         '<span id="zoom-minus" style="cursor:pointer;font-weight:bold;color: rgb(0, 255, 0);">-</span>' +
