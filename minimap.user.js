8fb5902a5860770bff72b4bc3ca0b77a0cd3b600
zAsuma <51724561+zAsuma@users.noreply.github.com>
2019-06-15 16:04:41 +0100

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -18,7 +18,7 @@ Number.prototype.between = function(a, b) {
   return this > min && this < max;
 };
 var range = 25;
-window.baseTepmlateUrl = 'https://raw.githubusercontent.com/zAsuma/ImperioMap/blob/master/';
+window.baseTepmlateUrl = 'https://raw.githubusercontent.com/zZz0mb/BRASIL-ZONE-MINIMAPA/master';
 
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
